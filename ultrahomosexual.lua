6684613e0b2d5a0ca34b10e59381431f97c30f9e
ak <77134924+Pikaruru@users.noreply.github.com>
2022-06-29 18:11:37 +0200

    Add files via upload

---
+++ ultrahomosexual.lua
@@ -0,0 +1,33 @@
+
+--made by kalas#1330 dont dm otherwise ill eat your ass
+local Mouse = game:GetService("Players").LocalPlayer:GetMouse()
+local Toggled = false
+local Keybind = "f" --keybind
+
+
+Mouse.KeyDown:Connect(function(Key)
+if Key == Keybind then
+if Toggled then
+Toggled = false
+else
+Toggled = true
+while task.wait() do
+if Toggled == true then
+pcall(function()
+for i,v in pairs(workspace.Mobs:GetDescendants()) do
+    if v:IsA("Model") then
+        local ohInstance1 = v
+        local ohVector32 = v.HumanoidRootPart.Position
+        game:GetService("ReplicatedStorage").Remotes.Gameplay.FireHit:FireServer(ohInstance1, ohVector32)
+        if v.Humanoid.Health == 0 then v:Destroy() end
+        if v == nil or Toggled == false then break end
+    end
+end
+end)
+end
+end
+end
+end
+end)
+
+
