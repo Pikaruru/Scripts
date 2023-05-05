bd38f624eaaeba7f7e14d7536cd1960fbee679cf
choke <47597850+choke-dev@users.noreply.github.com>
2023-05-05 15:56:03 +0800

    Update loadstringloaderyes.lua
    you suck

--- loadstringloaderyes.lua
+++ loadstringloaderyes.lua
@@ -1,99 +1,37 @@
-if game.PlaceId == 6551809638 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/farmingadventures.lua"))()
-else
-	if game.PlaceId == 9190926538 then
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/Toyfighters.lua"))()
-	else
-		if game.PlaceId == 5202297512 then
-			loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/bobasim.lua"))()
-		else
-			if game.PlaceId == 9386846196 then
-				loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/brawlinggrounds.lua"))()
-			else
-				if game.PlaceId == 9588998913 then
-					loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/empireclash.lua"))()
-				else
-					if game.PlaceId == 10070062081 then
-						loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/minion.lua"))()
-					else
-						if game.PlaceId == 2043040358 then
-							loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/vyyrahk.lua"))()
-						else
-							if game.PlaceId == 10325366821 then
-								loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/GuillotineTycoon.lua"))()
-							else
-								if game.PlaceId == 9894044804 then
-									loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/bladesimulator.lua"))()
-								else
-									if game.PlaceId == 4895427362 then
-										loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/romangaypire.lua"))()
-									else
-										if game.PlaceId == 10033287610 then
-											loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/smasherman.lua"))()
-										else
-											if game.PlaceId == 8585657145 then
-												loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/animeherosim.lua"))()
-											else
-												if game.PlaceId == 10108131074 then
-													loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/mowthelawn.lua"))()
-												else
-													if game.PlaceId == 8549047195 then
-														loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/aws.lua"))()
-													else
-														if game.PlaceId == 10011532704 then
-															loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/AnimeBrawlSimulator.lua"))()
-														else
-															if game.PlaceId == 8612195438 then
-																loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/clickerpartysimulator.lua"))()
-															else
-																if game.PlaceId == 7898006543 then
-																	loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/mushroomsimulator.lua"))()
-																else
-																	if game.PlaceId == 9927535243 then
-																		loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/minersgalore.lua"))()
-																	else
-																		if game.PlaceId == 7225720036 then
-																			loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/miningsaga.lua"))()
-																		else
-																			if game.PlaceId == 9157870347 then
-																				loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/petadventures.lua"))()
-																			else
-																				if game.PlaceId == 10106105124 then
-																					loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/wheatfarmingsim.lua"))()
-																				else
-																					if game.PlaceId == 10157366004 then
-																						loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/pettappingsimulator.lua"))()
-																					else
-																						if game.PlaceId == 9970551995 then
-																							loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/touchthegrass.lua"))()
-																						else
-																							if game.PlaceId == 10204162989 then
-																								loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/pickaxesim.lua"))()
-																							else
-																								if game.PlaceId == 10347946161 then
-																									loadstring(game:HttpGet("https://raw.githubusercontent.com/Pikaruru/Scripts/main/ratwash.lua"))()
-																								end
-																							end
-																						end
-																					end
-																				end
-																			end
-																		end
-																	end
-																end
-															end
-														end
-													end
-												end
-											end
-										end
-									end
-								end
-							end
-						end
-					end
-				end
-			end
-		end
-	end
+local MainRepository = "https://raw.githubusercontent.com/Pikaruru/Scripts/main/"
+
+local PlaceIDs = {
+    [6551809638] = "farmingadventures.lua",
+    [9190926538] = "Toyfighters.lua",
+    [5202297512] = "bobasim.lua",
+    [9386846196] = "brawlinggrounds.lua",
+    [9588998913] = "empireclash.lua",
+    [10070062081] = "minion.lua",
+    [2043040358] = "vyyrahk.lua",
+    [10325366821] = "GuillotineTycoon.lua" ,
+    [9894044804] = "bladesimulator.lua",
+    [4895427362] = "romangaypire.lua",
+    [10033287610] = "smasherman.lua",
+    [8585657145] = "animeherosim.lua",
+    [10108131074] = "mowthelawn.lua",
+    [8549047195] = "aws.lua",
+    [10011532704] = "AnimeBrawlSimulator.lua",
+    [8612195438] = "clickerpartysimulator.lua",
+    [7898006543] = "mushroomsimulator.lua",
+    [9927535243] = "minersgalore.lua",
+    [7225720036] = "miningsaga.lua",
+    [9157870347] = "petadventures.lua",
+    [10106105124] = "wheatfarmingsim.lua",
+    [10157366004] = "pettappingsimulator.lua",
+    [9970551995] = "touchthegrass.lua",
+    [10204162989] = "pickaxesim.lua",
+    [10347946161] = "ratwash.lua"
+}
+
+local Success, Result = pcall(function()
+    loadstring(game:HttpGet(MainRepository..PlaceIDs[game.PlaceId]))()
+end)
+
+if not Success then
+    warn("Error while loading script: "..Result)
 end
